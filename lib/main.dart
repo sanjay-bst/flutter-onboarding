59b1e08c9bbf4622e8bd0624dc68b8f8b7e7bf3f
sanjay-bst <sanjay_yadav@bluestacks.com>
2022-02-28 17:09:44 +0530

    resetting the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-import './router/router.dart';
-import './root.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(MyApp(
-    router: AppRouter(),
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final AppRouter router;
-
-  const MyApp({Key? key, required this.router}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Game.tv',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        appBarTheme: const AppBarTheme(
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-          iconTheme: IconThemeData(
-            color: Colors.black,
-          ),
-        ),
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const Root(),
-      onGenerateRoute: router.generateRoute,
+    return const Center(
+      child: Text('Flutter Onboarding...'),
     );
   }
 }
